58d1522fcbd0789b769ef155925623a8a15ef6fd
andrii <mantula.andrey@gmail.com>
2025-08-13 11:19:22 +0300

    fix: roll back

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from 'filters/http-exception.filter';
 import cookieParser from 'cookie-parser';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
-import serverless from 'serverless-http';
-
-const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressApp),
-  );
-  app.setGlobalPrefix('api');
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: ['http://localhost:5173'],
@@ -27,5 +18,3 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
-
-export const handler = serverless(expressApp);
